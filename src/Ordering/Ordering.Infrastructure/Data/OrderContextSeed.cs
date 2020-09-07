f271baed6298613d57aad3a64a25792155ddfe06
Harshavardhan Padidala <v-hapadi@microsoft.com>
2020-09-07 11:12:57 +0530

    adding ordercontext and creating orders table using migrations.

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext,ILoggerFactory loggerFactory,int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if(retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var logger = loggerFactory.CreateLogger<OrderContextSeed>();
+                    logger.LogError(ex.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() { UserName = "Harsha", FirstName = "Harsha", LastName = "v", EmailAddress = "har@gmail.com", AddressLine = "Hyd", Country = "India",TotalPrice = 5000 },
+            };
+        }
+    }
+}
