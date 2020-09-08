3247a48d7556bb204af91628262e55088eca195e
Harshavardhan Padidala <v-hapadi@microsoft.com>
2020-09-09 00:41:56 +0530

    models and api class changes added

---
+++ src/Client/WebApp/ApiCollection/OrderApi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp.ApiCollection.Infrastructure;
+using WebApp.ApiCollection.Interfaces;
+using WebApp.Models;
+using WebApp.Settings;
+
+namespace WebApp.ApiCollection
+{
+    public class OrderApi : BaseHttpClientWithFactory, IOrderApi
+    {
+        private readonly IApiSettings _settings;
+
+        public OrderApi(IHttpClientFactory factory, IApiSettings settings): base(factory)
+        {
+            _settings = settings;
+        }
+
+        public async Task<IEnumerable<OrderModel>> GetOrdersByUserName(string userName)
+        {
+            var message = new HttpRequestBuilder(_settings.BaseAddress)
+                           .SetPath(_settings.OrderPath)
+                           .AddQueryString("username", userName)
+                           .HttpMethod(HttpMethod.Get)
+                           .GetHttpMessage();
+
+            return await SendRequest<IEnumerable<OrderModel>>(message);
+        }
+    }
+}
---
+++ src/Client/WebApp/Models/BasketModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class CartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
